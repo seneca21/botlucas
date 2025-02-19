ed7960b7b9e5d7b3dba1a23b07a1505dc3642275
snc1 <pedroh.assis118@gmail.com>
2025-02-19 00:00:16 -0300

    .

--- models/Bot.js
+++ models/Bot.js
@@ -27,23 +27,11 @@ module.exports = (sequelize, DataTypes) => {
             remarketingJson: {
                 type: DataTypes.TEXT,
                 allowNull: true
-            },
-            // NOVO: Coluna para data de criação
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW
-            },
-            // NOVO: Coluna para data de atualização
-            updated_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW
             }
         },
         {
             tableName: 'Bots',  // Tabela "Bots"
-            timestamps: false   // Desabilitamos o timestamps nativo, pois estamos definindo as colunas manualmente
+            timestamps: false   // Se quiser createdAt/updatedAt, remova esta linha
         }
     );
 
