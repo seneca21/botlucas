3ea859387e41837cf230a741614df2295ab5d63d
snc1 <pedroh.assis118@gmail.com>
2025-02-19 10:32:55 -0300

    .

--- models/Bot.js
+++ models/Bot.js
@@ -1,38 +1,40 @@
-// models/Bot.js
 module.exports = (sequelize, DataTypes) => {
-    const Bot = sequelize.define('Bot', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
+    const Bot = sequelize.define(
+        'Bot',
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            token: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            description: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            vipLink: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            video: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            buttonsJson: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            remarketingJson: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            }
         },
-        token: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        video: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        buttonsJson: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        remarketingJson: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        vipLink: {                     // NOVO: campo para o link do grupo VIP
-            type: DataTypes.STRING,
-            allowNull: true
+        {
+            tableName: 'Bots',
+            timestamps: false
         }
-    }, {
-        tableName: 'Bots',
-        timestamps: false
-    });
-
+    );
     return Bot;
 };
