fa3ab5ca6fdeaeca4f7050c166ed8ece1f96313e
snc1 <pedroh.assis118@gmail.com>
2025-02-27 18:31:47 -0300

    .

--- services/index.js
+++ services/index.js
@@ -5,9 +5,9 @@ const logger = require('./logger'); // se necessário
 
 // Cria a conexão com o banco de dados com as novas credenciais
 const sequelize = new Sequelize(
-  'd4tbpmqtecpvg3',          // Nome do DB
-  'u8iqsvi6vhfqam',          // Usuário
-  'pc59b97f3a93f97655eeb66088501a3c29c1e20d9b01f4c156beed3a7d4bc16e8', // Senha
+  'deefe35i9plvl7',          // Nome do DB
+  'ucahdtofln6o1c',          // Usuário
+  'p5091a792840333d7cb39c5cf8461d5c9b2a223f6db70b93287977e0ba152229b', // Senha
   {
     host: 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
     port: 5432,
