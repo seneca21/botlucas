64307645bac1c49cf6665fa4768c62f599ef8d83
snc1 <pedroh.assis118@gmail.com>
2025-04-07 15:02:27 -0300

    .

--- app.js
+++ app.js
@@ -154,8 +154,8 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
-    const ADMIN_USER = "perufe";
-    const ADMIN_PASS = "oppushin1234";
+    const ADMIN_USER = "lucas";
+    const ADMIN_PASS = "lucas123";
 
     if (username === ADMIN_USER && password === ADMIN_PASS) {
         req.session.loggedIn = true;
